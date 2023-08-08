c59c2f740dabc3b93239decd2eacec67f0f2d1f3
Chris Pennington <chris@codinginpublic.dev>
2023-08-08 09:41:02 -0600

    add view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
   site: "https://astro-blog-cip.netlify.app",
   experimental: {
     assets: true,
+    viewTransitions: true,
   },
   image: {
     service: sharpImageService(),
